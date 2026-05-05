345ca6c4224b317c210300e7b41de8736bc9ef37
Timofey Kuznetsov <tima1702@gmail.com>
2026-05-05 09:13:05 +0300

    chore: drop vite boilerplate, set up ru locale and bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+import { FormProvider } from './context/FormContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <FormProvider>
+        <App />
+      </FormProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
